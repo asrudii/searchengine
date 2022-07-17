acdefbb8ffea0db3aaa1ab2e48b823977297dc87
asrudii <aseprudi.luqmanul@gmail.com>
2022-07-17 14:48:28 +0700

    api link

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+export const API_SEARCH_URL = 'https://google-search3.p.rapidapi.com/api/v1/search/'
+export const API_NEWS_SEARCH_URL = 'https://google-search3.p.rapidapi.com/api/v1/news/'
+export const API_IMAGE_SEARCH_URL = 'https://google-search3.p.rapidapi.com/api/v1/image'
